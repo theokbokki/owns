9cae0666937473e35604bc8be994141b2b596027
théoo <90846584+theokbokki@users.noreply.github.com>
2025-07-12 23:50:10 +0200

    Add a bunch of items

--- public/index.php
+++ public/index.php
@@ -15,6 +15,56 @@ $items = [
         'clear-casio.webp',
         'A casio digital watch with a clear bracelet and a white watch face'
     ),
+    Item::make(
+        'Green iphone 11', 
+        'iphone-11.webp',
+        'A front facing green iphone 11'
+    ),
+    Item::make(
+        'Nintendo Switch 2', 
+        'nintendo-switch-2.webp',
+        'A Nintendo Switch 2 with the game Mario Kart world'
+    ),
+    Item::make(
+        'Green armchair', 
+        'green-armchair.webp',
+        'A bright green armchair from IKEA. It\'s rounded and plush with a metal feet'
+    ),
+    Item::make(
+        'Red armchair', 
+        'red-armchair.webp',
+        'A bright red armchair from IKEA. It\'s got wooden armrests that extend as a base'
+    ),
+    Item::make(
+        'Air pods 2', 
+        'airpods-2.webp',
+        'The charging box of air pods 2 seen from the back'
+    ),
+    Item::make(
+        'Nike shoes with green swoosh', 
+        'nike-green-swoosh.webp',
+        'A pair of white Nike shoes with green swoosh and sole'
+    ),
+    Item::make(
+        'Green Imac + keyboard and mouse', 
+        'green-imac.webp',
+        'A green 2021 apple Imac with the matching magic mouse and matching keyboard'
+    ),
+    Item::make(
+        'Climax — Rage against the facism', 
+        'climax-ratf.webp',
+        'A orange magazine cover titled &quot;Climax Rage Against The Facism&quot;. The word fascism is breaking a Tesla Cyber Truck in half. There is some subtext under the Cyber Truck'
+    ),
+    Item::make(
+        'DAK—Saint-Tropez', 
+        'dak-saint-tropez.webp',
+        'A yellow and white striped cardboard cofee box. On top is a white rectangular sticker that seals the box with Saint-Tropez written on it. On the front of the box is another sticker of a drawing of a person drinking a cup of coffee looking satisfied'
+    ),
+    Item::make(
+        'La vie secrète des arbres—Peter Wohlleben', 
+        'la-vie-secrete-des-arbres.webp',
+        'The cover of the book &quot;La vie Secrète des Arbres&quot; by Peter Wohlleben. The background image is a fores. The name of the author is written in yellow above the title in white. At the bottom is a book banner with some text promoting the book and the amount of readers it got.'
+    ),
 ];
 
 ?>
@@ -31,7 +81,7 @@ $items = [
     </head>
     <body class="app">
         <h1 class="app__title">Owns</h1>
-        <p class="app__description">The place where I put pictures of stuff that I own and that I like. It's a bit like a digital inventory for my real life self.</p>
+        <p class="app__description">The place where I put pictures of stuff that I own and that I like. It's a bit like a digital inventory for my real life self.<br>Some of the stuff is consumable, so I might not have it anymore.</p>
         <div class="app__items">
             <?php foreach($items as $item): ?> 
                 <?= $item->render() ?>
