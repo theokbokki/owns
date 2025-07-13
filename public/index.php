2a057e0d596bb0201da6c8087deac1f7f28502f1
théoo <90846584+theokbokki@users.noreply.github.com>
2025-07-13 09:27:52 +0200

    Add meta description and favicon

--- public/index.php
+++ public/index.php
@@ -80,7 +80,14 @@ $items = [
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" type="image/png" href="<?= asset('favicon/favicon-96x96.png') ?>" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="<?= asset('favicon/favicon.svg') ?>" />
+        <link rel="shortcut icon" href="<?= asset('favicon/favicon.ico') ?>" />
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= asset('favicon/apple-touch-icon.png') ?>" />
+        <link rel="manifest" href="<?= asset('favicon/site.webmanifest') ?>" />
+
         <title>Owns</title>
+        <meta name="description" content="The place where I put pictures of stuff that I own and that I like. It's a bit like a digital inventory for my real life self. Some of the stuff is consumable, so I might not have it anymore." />
 
         <link rel="stylesheet" href="<?= asset('css/app.css') ?>">
     </head>
